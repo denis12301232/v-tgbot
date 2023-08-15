20564518fcf64186ef7decf38b09e91f3e52f63f
Denis <denis12301232@gmail.com>
2023-08-15 13:17:35 +0300

    add:graceful shutdown

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { factory } from '@/factory.js';
 import fastify from 'fastify';
 
 const app = fastify({ logger: true });
+process.on('SIGTERM', onShutDown);
+process.on('SIGINT', onShutDown);
+
+main();
 
 async function main() {
   try {
@@ -13,4 +17,7 @@ async function main() {
   }
 }
 
-main();
+async function onShutDown() {
+  await app.close();
+  process.exit(0);
+}
